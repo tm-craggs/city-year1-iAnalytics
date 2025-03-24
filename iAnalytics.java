b601aa42e45494c1261df94798594c0b5294fe18
Thomas Craggs <Thomas.Craggs@city.ac.uk>
2025-03-24 09:21:40 +0000

    inital commit

---
+++ iAnalytics.java
@@ -0,0 +1,64 @@
+/**
+ * Class of operations on integer arrays.
+ * You MUST NOT change the signatures of the methods supplied. 
+ */
+ 
+// IN1002 Introduction to Algorithms
+// Coursework 2024/2025
+//
+// Submission by
+// YOUR_NAME_GOES_HERE
+// YOUR_EMAIL_GOES_HERE
+
+public class iAnalytics {
+
+    
+	// Task 1: Count unique elements in an ordered array
+    public int countUnique(int[] arr) {
+        // replace the following line with your implementation
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+
+    // Task 2: Find least frequent value in an ordered array
+    public int leastFrequent(int[] arr) {
+        // replace the following line with your implementation
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+
+    // Task 3: Count elements in an ordered array less than num
+    public int countLess(int[] arr, int num) {
+        // replace the following line with your implementation
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+	
+
+    // Task 4: Count elements in an ordered array between low and high
+    public int countBetween(int[] arr, int low, int high) {
+        // replace the following line with your implementation
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+	
+
+    // Task 5: Find top K most frequent elements in an ordered array
+    public int[] topKFrequent(int[] arr, int k) {
+        // replace the following line with your implementation
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    // Task 6: Longest contiguous subarray in ascending order
+    public int[] longestAscSubarray(int[] arr) {
+        // replace the following line with your implementation
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+
+    // Task 7: Maximum sum of a contiguous subarray with exactly k elements
+    public int maxSubarraySum(int[] arr, int k) {
+        // replace the following line with your implementation
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+
+}
